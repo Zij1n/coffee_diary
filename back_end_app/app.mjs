c2136488f4d0c940f13cc85cfa858a8b1b36cd86
Zijin Hu <zh2025@nyu.edu>
2022-11-08 14:22:24 -0500

    set proxy

--- back_end_app/app.mjs
+++ back_end_app/app.mjs
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(cors());
+app.use(cors());    
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
