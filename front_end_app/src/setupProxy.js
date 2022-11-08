26427d21fbea51356f8a2f376c363205756ecd1f
root <root@vultr.guest>
2022-11-08 20:47:46 +0000

    working version on vps

--- front_end_app/src/setupProxy.js
+++ front_end_app/src/setupProxy.js
@@ -2,10 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/recipe',
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
+    createProxyMiddleware('/recipes', {
+	    target: 'http://localhost:5000',
       changeOrigin: true,
     })
-  );
+  )
+   app.use(
+    createProxyMiddleware('/create', {
+            target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  )
 };
